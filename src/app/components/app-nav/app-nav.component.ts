d14d631316513e9da0419f2771e22cdb1dc0dbc0
JoepKockelkorn <joep.kockelkorn@qnh.nl>
2018-03-10 14:35:05 +0100

    created nav and header and logout functionality

---
+++ src/app/components/app-nav/app-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  styleUrls: ['app-nav.component.scss'],
+  templateUrl: 'app-nav.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppNavComponent {
+  constructor() {}
+}
